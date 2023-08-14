5398fe779c7f47f52bd609751017bc69e0be453e
Matt <brasil.mateus748@gmail.com>
2023-08-14 13:17:15 -0300

    Added function operate

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-function add(a, b) {
-
+function add() {
+let a 
+let b
+let c
 }
 
 function subtract(a, b) {
-    
+    let a 
+    let b
+    let c
 }
 
 function multiply(a, b) {
-    
+    let a 
+    let b
+    let c
 }
 
 function divide(a, b) {
-    
+    let a 
+    let b
+    let c
+}
+
+function operate(a, b) {
+if(a + b) return add();
+if(a - b) return subtract();
+if(a * b) return multiply();
+if(a / b) return divide();
 }
